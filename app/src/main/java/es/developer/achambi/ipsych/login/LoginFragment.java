25126db3c284e246887441d9b9c898fddf597eef
AlvaroChambi <alvaro.chambi.campos@gmail.com>
2018-12-20 11:32:30 +0100

    Migrated login/logout flow

--- app/src/main/java/es/developer/achambi/ipsych/login/LoginFragment.java
+++ app/src/main/java/es/developer/achambi/ipsych/login/LoginFragment.java
@@ -1,70 +1,13 @@
 package es.developer.achambi.ipsych.login;
 
 import android.content.Intent;
-import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 
-import com.firebase.ui.auth.AuthUI;
-import com.firebase.ui.auth.IdpResponse;
-import com.google.firebase.auth.FirebaseAuth;
-
-import java.util.Arrays;
-import java.util.List;
-
-import es.developer.achambi.coreframework.ui.BaseFragment;
+import es.developer.achambi.coreframework.ui.login.BaseLoginFragment;
 import es.developer.achambi.ipsych.NavigationActivity;
-import es.developer.achambi.ipsych.R;
 
-import static android.app.Activity.RESULT_OK;
-
-public class LoginFragment extends BaseFragment implements View.OnClickListener {
-    private static final int SIGN_IN_REQUEST_CODE = 101;
-
+public class LoginFragment extends BaseLoginFragment {
     @Override
-    public int getLayoutResource() {
-        return R.layout.login_fragment_layout;
-    }
-
-    @Override
-    public void onViewSetup(View view, @Nullable Bundle savedInstanceState) {
-        View loginButton = view.findViewById(R.id.login_button);
-        loginButton.setOnClickListener(this);
-
-        if( FirebaseAuth.getInstance().getCurrentUser() != null ) {
-            startActivity( new Intent(getActivity(), NavigationActivity.class) );
-            getActivity().finish();
-        }
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if( requestCode == SIGN_IN_REQUEST_CODE ) {
-            IdpResponse response = IdpResponse.fromResultIntent( data );
-
-            if( resultCode == RESULT_OK ) {
-                startActivity( new Intent(getActivity(), NavigationActivity.class) );
-                getActivity().finish();
-            } else {
-                if( response != null ){
-                    Snackbar snackbar = Snackbar.make( getView(),
-                            response.getError().getMessage(), Snackbar.LENGTH_INDEFINITE );
-                    snackbar.show();
-                }
-            }
-        }
-    }
-
-    @Override
-    public void onClick(View view) {
-        List<AuthUI.IdpConfig> providers = Arrays.asList(
-                new AuthUI.IdpConfig.EmailBuilder().build(),
-                new AuthUI.IdpConfig.GoogleBuilder().build() );
-        startActivityForResult( AuthUI.getInstance()
-                .createSignInIntentBuilder()
-                .setAvailableProviders( providers )
-                .build(), SIGN_IN_REQUEST_CODE);
+    public Intent getNextScreenIntent() {
+        return new Intent( getActivity(), NavigationActivity.class );
     }
 }
--- app/src/main/java/es/developer/achambi/ipsych/profile/ProfileFragment.java
+++ app/src/main/java/es/developer/achambi/ipsych/profile/ProfileFragment.java
@@ -1,73 +1,18 @@
 package es.developer.achambi.ipsych.profile;
 
-import android.graphics.drawable.Drawable;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.content.Intent;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.transition.Transition;
-import com.google.firebase.auth.FirebaseAuth;
-
-import es.developer.achambi.coreframework.ui.Presenter;
-import es.developer.achambi.ipsych.NavigationFragment;
-import es.developer.achambi.ipsych.R;
+import es.developer.achambi.coreframework.ui.profile.BaseProfileFragment;
 import es.developer.achambi.ipsych.login.LoginActivity;
 
-public class ProfileFragment extends NavigationFragment implements View.OnClickListener {
+public class ProfileFragment extends BaseProfileFragment {
     public static ProfileFragment newInstance() {
         return new ProfileFragment();
     }
 
-    private ProfilePresenter presenter;
-
     @Override
-    public int getLayoutResource() {
-        return R.layout.profile_fragment_layout;
-    }
-
-    @Override
-    public Presenter setupPresenter() {
-        if( presenter == null ) {
-            presenter = new ProfilePresenter();
-        }
-        return presenter;
-    }
-
-    @Override
-    public void onViewSetup(View view, @Nullable Bundle savedInstanceState) {
-        View logoutButton = view.findViewById(R.id.profile_logout_button);
-        final TextView profileName = view.findViewById(R.id.profile_user_name_text);
-        ImageView profilePhoto = view.findViewById(R.id.profile_photo_image_view);
-        profileName.setText( presenter.getProfileName() );
-        Glide.with(getActivity())
-                .load(presenter.getPhotoUrl())
-                .apply(RequestOptions.circleCropTransform())
-                .into(profilePhoto);
-        logoutButton.setOnClickListener(this);
-    }
-
-    @Override
-    public int getTitleResource() {
-        return R.string.profile_activity_title;
-    }
-
-    @Override
-    public void onClick(View view) {
-        if( view.getId() == R.id.profile_logout_button ) {
-            FirebaseAuth.getInstance().signOut();
-            startActivity(LoginActivity.getStartIntent( getActivity() ) );
-            getActivity().finish();
-        }
-    }
-
-    @Override
-    public void onDestroyView() {
-        super.onDestroyView();
+    public void onUserLoggedOut() {
+        startActivity( new Intent(getActivity(), LoginActivity.class));
+        getActivity().finish();
     }
 }
